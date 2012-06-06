79ea925dd8cead94f0cb6ec98e63db1ee8c114bf
Robin Böning <robin@magiclabs.de>
2012-06-06 17:41:50 +0200

    Using the new cache_path method

--- app/models/page_extension.rb
+++ app/models/page_extension.rb
@@ -4,5 +4,9 @@
 module Alchemy
 	Page.class_eval do
 		scope :current_domain, lambda{ |domain_id| includes(:language => :localizations).where('alchemy_localizations.domain_id' => domain_id) }
+
+		def self.cache_path(params, request)
+			"alchemy/#{request.host}/#{params[:lang]}/#{params[:urlname]}".gsub(/\/\//, '/')
+		end
 	end
 end
