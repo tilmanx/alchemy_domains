6929594da90d0255d3e656382d4dc5dd8187a16d
Robin Böning <robin@magiclabs.de>
2012-06-18 19:09:50 +0200

    Uses cache_key instead of old Page.cache_path

--- app/models/page_extension.rb
+++ app/models/page_extension.rb
@@ -5,8 +5,8 @@ module Alchemy
   Page.class_eval do
     scope :current_domain, lambda{ |domain_id| includes(:language => :localizations).where('alchemy_localizations.domain_id' => domain_id) }
 
-    def self.cache_path(params, request)
-      "alchemy/#{request.host}/#{params[:lang]}/#{params[:urlname]}".gsub(/\/\//, '/')
+    def cache_key(request = nil)
+      "alchemy/#{request.try(:host)}/#{language_code}/#{urlname}".gsub(/\/\//, '/')
     end
   end
 end
