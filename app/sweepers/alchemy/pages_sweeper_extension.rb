6a0ed7f1d4b6af417ed839352a08944b9a120632
Robin Böning <robin@magiclabs.de>
2012-06-18 19:51:03 +0200

    Using page.cache_key for expiring the cache.

--- app/sweepers/alchemy/pages_sweeper_extension.rb
+++ app/sweepers/alchemy/pages_sweeper_extension.rb
@@ -6,25 +6,8 @@ module Alchemy
 
     def expire_page(page)
       return if page.do_not_sweep
-      # TODO: We should change this back to expire_action after Rails 3.2 was released.
-      # expire_action(
-      #   alchemy.show_page_url(
-      #     :urlname => page.urlname_was,
-      #     :lang => multi_language? ? page.language_code : nil
-      #   )
-      # )
-      # Temporarily fix for Rails 3 bug
-      return if alchemy.nil?
       page.language.domains.each do |domain|
-        expire_fragment(ActionController::Caching::Actions::ActionCachePath.new(
-          self,
-          alchemy.show_page_url(
-            :urlname => page.urlname_was,
-            :lang => domain.languages.count > 1 ? page.language_code : nil,
-            :host => domain.hostname
-          ),
-          false
-        ).path)
+        expire_action(page.cache_key(OpenStruct.new(:host => domain.hostname)))
       end
     end
 
